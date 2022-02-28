26eea71b5211260e6a0687ac5325847239abdfce
Tatevik Ter-Hovhannisyan <aptahome@gmail.com>
2022-02-28 18:41:21 +0400

    made signal handling without getting user name

---
+++ homework_6/signal_handling.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <ucontext.h>
+
+void handler(int signum, siginfo_t* info, void* context_) {
+    if(signum != SIGUSR1) {
+        return;
+    }
+    ucontext_t* context = (ucontext_t*) context_;
+    std::cout << "Got signal " << signum << std::endl;
+    std::cout << "from PID: " << (unsigned int) info->si_pid << std::endl;
+    std::cout << "from PID: " << info->si_pid << std::endl;
+    std::cout << "UID: " << info->si_uid << std::endl;
+    std::cout << "User name: " << "???" << std::endl;                           // ???
+    std::cout << "EIP: " << context->uc_mcontext.gregs[REG_RIP] << std::endl;
+    std::cout << "EAX: " << context->uc_mcontext.gregs[REG_RAX] << std::endl;           
+    std::cout << "EBX: " << context->uc_mcontext.gregs[REG_RBX] << std::endl;           
+    exit(0);
+}
+
+int main() {
+    struct sigaction action;
+    
+    memset(&action, 0, sizeof(action));
+
+    action.sa_sigaction = handler;
+
+    // redefining action of SIGUSR1
+    int result = sigaction(SIGUSR1, &action, NULL);
+    // error handling
+    if(result < 0) {
+        std::cerr << "Error with sigaction " << strerror(errno) << std::endl;
+        exit(errno);
+    }
+    
+    while(true) {
+        sleep(10);
+    }
+    return 0;
+}
