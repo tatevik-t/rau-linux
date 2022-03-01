21dc3aeb60851c0619d549900b03b0ede34a2eed
Tatevik Ter-Hovhannisyan <aptahome@gmail.com>
2022-03-01 18:55:08 +0400

    solved problem with uid and username

--- homework_6/signal_handling.cpp
+++ homework_6/signal_handling.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <ucontext.h>
+#include <pwd.h>
 
 void handler(int signum, siginfo_t* info, void* context_) {
     if(signum != SIGUSR1) {
@@ -12,9 +13,12 @@ void handler(int signum, siginfo_t* info, void* context_) {
     ucontext_t* context = (ucontext_t*) context_;
     std::cout << "Got signal " << signum << std::endl;
     std::cout << "from PID: " << (unsigned int) info->si_pid << std::endl;
-    std::cout << "from PID: " << info->si_pid << std::endl;
     std::cout << "UID: " << info->si_uid << std::endl;
-    std::cout << "User name: " << "???" << std::endl;                           // ???
+    struct passwd *pws;
+    pws = getpwuid(info->si_uid);
+    if (pws != NULL) {
+        std::cout << "User name: " <<  pws->pw_name << std::endl;
+    }
     std::cout << "EIP: " << context->uc_mcontext.gregs[REG_RIP] << std::endl;
     std::cout << "EAX: " << context->uc_mcontext.gregs[REG_RAX] << std::endl;           
     std::cout << "EBX: " << context->uc_mcontext.gregs[REG_RBX] << std::endl;           
@@ -25,8 +29,9 @@ int main() {
     struct sigaction action;
     
     memset(&action, 0, sizeof(action));
-
-    action.sa_sigaction = handler;
+    
+    action.sa_sigaction = &handler;
+    action.sa_flags = SA_SIGINFO;
 
     // redefining action of SIGUSR1
     int result = sigaction(SIGUSR1, &action, NULL);
